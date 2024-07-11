2d19bd340b161b9696d818d8e31b4a0ba5fa45bc
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-07-11 13:44:07 +0700

    ✨ feat: add api to return environment name

--- src/config.ts
+++ src/config.ts
@@ -6,15 +6,18 @@ type Config = {
   PORT: number;
   MONGODB_URI: string;
   DATABASE_NAME: string;
+  NODE_ENV: string;
 };
 
 function getConfig(): Config {
   const port = Number(process.env.PORT ?? 4000);
+  const nodeEnv = process.env.NODE_ENV ?? 'development';
   const mongodbUri = process.env.MONGODB_URI ?? '';
   const databaseName = process.env.DATABASE_NAME ?? 'test';
 
   return {
     PORT: port,
+    NODE_ENV: nodeEnv,
     MONGODB_URI: mongodbUri,
     DATABASE_NAME: databaseName,
   };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,12 @@ import config from '../config';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.status(200).send(`OK, ${config.NODE_ENV.toUpperCase()}:${config.PORT}`);
+});
+
 router.get('/health', (req, res) => {
-  res.status(200).send(`OK ${config.PORT}`);
+  res.status(200).send(`OK, ${config.PORT}`);
 });
 
 export default router;
